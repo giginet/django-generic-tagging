bdac6a483c656480bad56ce442aadb02674adac3
giginet <giginet.net@gmail.com>
2016-03-22 23:20:42 +0900

    Skip migrations when test is running

--- tests/settings.py
+++ tests/settings.py
@@ -101,3 +101,14 @@ AUTH_USER_MODEL = 'auth.User'
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
+
+# https://gist.github.com/NotSqrt/5f3c76cd15e40ef62d09
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
+
+MIGRATION_MODULES = DisableMigrations()

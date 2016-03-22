422dce1e0906d2a0521dab5944f20586a9e87e7b
giginet <giginet.net@gmail.com>
2016-03-22 22:52:31 +0900

    Add generic_tagging to example project

--- example/generic_tagging_example/urls.py
+++ example/generic_tagging_example/urls.py
@@ -5,6 +5,7 @@ from generic_tagging_example.blog.views import ArticleListView, ArticleDetailVie
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tags/', include('generic_tagging.urls')),
     url(r'^(?P<pk>\d+)/$',
         ArticleDetailView.as_view(), name='blog_article_detail'),
     url(r'^$', ArticleListView.as_view(), name='blog_article_list'),
